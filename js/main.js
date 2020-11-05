79ac432a54a352f4873513463e249d1b56dc7353
kirillstarkov <xmdnssx@gmail.com>
2020-11-05 16:59:48 +0200

    5

--- js/main.js
+++ js/main.js
@@ -2,40 +2,47 @@ $(document).ready(function(){
   $('.slider-fade').slick({
   	speed: 500,
   	fade: true,
-  	cssEase: 'linear',
   	arrows: false,
-  	dots: true
+  	dots: true,
+  	responsive: [{
+  		breakpoint: 768,
+  		settings: {
+  			arrows: true,
+  			dots: true,
+  		}
+  	}]
   });
 
   $('.sldr').slick({
   	speed: 500,
   	arrows: false,
-  	slidesToShow: 1
-  	// responsive: [
-  	// {
-  	// 	breakpoint: 460,
-  	// 	settings: {
-  	// 		arrows: false,
-  	// 		dots: true,
-  	// 		slidesToShow: 1
-  	// 	}
-  	// },
-  	// {
-  	// 	breakpoint: 768,
-  	// 	settings: {
-  	// 		arrows: false,
-  	// 		dots: true,
-  	// 		slidesToShow: 2
-  	// 	}
-  	// }
-  	// {
-  	// 	breakpoint: 1024,
-  	// 	settings: {
-  	// 		arrows: true,
-  	// 		dots: false,
-  	// 		slidesToShow: 4
-  	// 	}
-  	// }
-  	// ]
+  	slidesToShow: 4,
+
+  	responsive: [
+  	{
+  		breakpoint: 460,
+  		settings: {
+  			arrows: false,
+  			dots: true,
+  			slidesToShow: 1
+  		}
+  	},
+  	{
+  		breakpoint: 768,
+  		settings: {
+  			arrows: false,
+  			dots: true,
+  			slidesToShow: 2
+  		}
+  	},
+  	{
+  		breakpoint: 1024,
+  		settings: {
+  			arrows: true,
+  			dots: false,
+  			slidesToShow: 4
+  		}
+  	},
+  	]
   });
 });
